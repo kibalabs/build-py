43057739e4dd60023a3a69a0bbb3d26c26b2d433
Krishan Patel <krishan711@gmail.com>
2021-05-17 13:58:13 +0100

    .

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+setupDirectory = os.path.dirname(os.path.realpath(__file__))
+
+requirements = []
+with open(os.path.join(setupDirectory, 'requirements.txt'), 'r') as requirementsFile:
+    for requirement in requirementsFile.read().splitlines():
+        if requirement:
+            requirements.append(requirement)
+
+setup(
+    name='kiba-build',
+    version='0.1.0',
+    description='Kiba Labs\' python building and testing utilities',
+    url='https://github.com/kibalabs/build-py',
+    packages=find_packages(exclude=['tests*']),
+    python_requires='~=3.7',
+    install_requires=requirements,
+    tests_require=[],
+    package_data={
+        'core': [
+            'py.typed',
+        ]
+    },
+    test_suite='tests',
+    include_package_data=True,
+    extras_require={
+    },
+)
