69c48e4a417dca67c13b99c85b0911bcc22f0c57
Krishan Patel <krishan711@gmail.com>
2022-01-26 16:30:37 +0000

    .

--- buildpy/version.py
+++ buildpy/version.py
@@ -8,7 +8,7 @@ import click
 @click.option('-c', '--count', 'count', required=False, type=int, default=1)
 def run(part: str, count: int) -> None:
     for _ in range(count):
-        subprocess.check_output(f"bump2version --allow-dirty --current-version \"$(python setup.py --version)\" --parse '(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)(\.dev(?P<dev>\d+))?' --serialize '{{major}}.{{minor}}.{{patch}}.dev{{dev}}' --serialize '{{major}}.{{minor}}.{{patch}}' {part} setup.py", stderr=subprocess.STDOUT, shell=True)
+        subprocess.check_output(f"bump2version --allow-dirty --current-version \"$(python setup.py --version)\" --parse '(?P<major>\\d+)\\.(?P<minor>\\d+)\\.(?P<patch>\\d+)(\\.dev(?P<dev>\\d+))?' --serialize '{{major}}.{{minor}}.{{patch}}.dev{{dev}}' --serialize '{{major}}.{{minor}}.{{patch}}' {part} setup.py", stderr=subprocess.STDOUT, shell=True)
 
 if __name__ == '__main__':
     run()  # pylint: disable=no-value-for-parameter
