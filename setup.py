9016acde36929cb09ecc2b7f47a90ab4bc811571
Krishan Patel <krishan711@gmail.com>
2022-07-19 16:47:32 +0100

    v0.1.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(setupDirectory, 'requirements.txt'), 'r') as requirements
 
 setup(
     name='kiba-build',
-    version='0.1.4',
+    version='0.1.5',
     description='Kiba Labs\' python building and testing utilities',
     url='https://github.com/kibalabs/build-py',
     packages=find_packages(exclude=['tests*']),
