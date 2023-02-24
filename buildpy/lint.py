e1d48748b1bfc6beac307a277035926cbc9a2c4b
Krishan Patel <krishan711@gmail.com>
2023-02-24 10:12:22 +0000

    .

--- buildpy/lint.py
+++ buildpy/lint.py
@@ -1,5 +1,8 @@
 import os
-from typing import List
+import json
+import subprocess
+import re
+from typing import List, Any, Dict
 
 import click
 from pylint.lint import Run as run_pylint  # type: ignore[import]
@@ -39,19 +42,49 @@ class PylintMessageParser(CollectingReporter, MessageParser):
         return output
 
 
+class RuffMessageParser(MessageParser):
+
+    def parse_json_messages(self, rawMessages: List[Dict[str, Any]]) -> List[Message]:
+        output: List[Message] = []
+        for rawMessage in rawMessages:
+            output.append(Message(
+                path=rawMessage['filename'],
+                line=rawMessage['location']['row'],
+                column=rawMessage['location']['column'],
+                code=rawMessage['code'],
+                text=rawMessage['message'],
+                level='error',
+            ))
+        return output
+
+
 @click.command()
 @click.option('-d', '--directory', 'directory', required=False, type=str)
 @click.option('-o', '--output-file', 'outputFilename', required=False, type=str)
 @click.option('-f', '--output-format', 'outputFormat', required=False, type=str, default='pretty')
 @click.option('-c', '--config-file-path', 'configFilePath', required=False, type=str)
-def run(directory: str, outputFilename: str, outputFormat: str, configFilePath: str) -> None:
+@click.option('-n', '--new', 'shouldUseNewVersion', default=False, is_flag=True)
+@click.option('-x', '--fix', 'shouldFix', default=False, is_flag=True)
+def run(directory: str, outputFilename: str, outputFormat: str, configFilePath: str, shouldUseNewVersion: bool, shouldFix: bool) -> None:
     currentDirectory = os.path.dirname(os.path.realpath(__file__))
     targetDirectory = os.path.abspath(directory or os.getcwd())
-    pylintConfigFilePath = configFilePath or f'{currentDirectory}/pylintrc'
-    pylintMessageParser = PylintMessageParser()
-    run_pylint([f'--rcfile={pylintConfigFilePath}', targetDirectory], reporter=pylintMessageParser, exit=False)
+    # NOTE(krishan711): track ruff called from python: https://github.com/charliermarsh/ruff/issues/659
+    if shouldUseNewVersion:
+        ruffConfigFilePath = configFilePath or f'{currentDirectory}/ruff.toml'
+        rawMessages = []
+        try:
+            subprocess.check_output(f'ruff check --format json --config {ruffConfigFilePath} {"--fix" if shouldFix else ""} {targetDirectory}', stderr=subprocess.STDOUT, shell=True)  # nosec=subprocess_popen_with_shell_equals_true
+        except subprocess.CalledProcessError as exception:
+            rawMessages = json.loads(exception.output.decode())
+        messageParser = RuffMessageParser()
+        messages = messageParser.parse_json_messages(rawMessages=rawMessages)
+    else:
+        pylintConfigFilePath = configFilePath or f'{currentDirectory}/pylintrc'
+        messageParser = PylintMessageParser()
+        run_pylint([f'--rcfile={pylintConfigFilePath}', f'--jobs=0', targetDirectory], reporter=messageParser, exit=False)
+        messages = messageParser.get_messages()
     reporter = GitHubAnnotationsReporter() if outputFormat == 'annotations' else PrettyReporter()
-    output = reporter.create_output(messages=pylintMessageParser.get_messages())
+    output = reporter.create_output(messages=messages)
     if outputFilename:
         with open(outputFilename, 'w') as outputFile:
             outputFile.write(output)
